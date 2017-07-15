bd9af7e8adadc4c28ca3903e6ee3d8586314f7a7
pyclear <cappyclear@gmail.com>
2017-07-15 12:06:05 +0800

    remove thems

--- themes/even/source/js/src/bootstrap.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  Even.fancybox.register();
-  Even.mobileNavbar.register();
-  Even.search.register();
-  Even.postToc.register();
-  Even.backToTop.register();
-})
